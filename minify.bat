24f21751f31ef514609b615d4b36753613c1b583
mperdeck <mperdeck@gmail.com>
2014-02-26 20:21:28 +1100

    Moved all files into the root directory

--- minify.bat
+++ minify.bat
@@ -1,5 +1,5 @@
 # Creates .min.js files for all .js files
 
-"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "C:\Program Files (x86)\Java\jars\Google Closure Compiler\compiler.jar" --js Sources/jsnlog.js --js_output_file=Minified/jsnlog.min.js
+"C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar "C:\Program Files (x86)\Java\jars\Google Closure Compiler\compiler.jar" --js jsnlog.js --js_output_file=jsnlog.min.js
 
 
