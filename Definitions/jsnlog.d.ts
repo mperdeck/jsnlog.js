0b2042d712cd17a017b55ffb99bac243abcf0d96
Matt Perdeck <mperdeck@gmail.com>
2016-07-31 00:12:11 +1000

    2.19.0

--- Definitions/jsnlog.d.ts
+++ Definitions/jsnlog.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for JSNLog v2.18.0+
+// Type definitions for JSNLog v2.19.0+
 // Project: https://github.com/mperdeck/jsnlog.js
 // Definitions by: Mattijs Perdeck <https://github.com/mperdeck>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
