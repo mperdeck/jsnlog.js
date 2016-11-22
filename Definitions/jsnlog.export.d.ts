e4efbcbfc90fa55fe87714a1862bbc6f3609e6c3
Matt Perdeck <mperdeck@gmail.com>
2016-11-22 23:56:58 +1100

    fix export statement, so JL can be imported as per the doco on jsnlog.com

--- Definitions/jsnlog.export.d.ts
+++ Definitions/jsnlog.export.d.ts
@@ -2,4 +2,4 @@
 // This file is listed in the typings element in the package.json file.
 // To make that work, this file must be recognized as a "module".
 // For this, the file must have an export statement.
-export default JL;
+export { JL };
