f67562f9e688d545847a6ec053aabb29cd8a1e80
Matt Perdeck <mperdeck@gmail.com>
2016-11-24 22:29:04 +1100

    fix export statement in generated code

--- Definitions/jl.d.ts
+++ Definitions/jl.d.ts
@@ -117,4 +117,4 @@ declare namespace JL {
 // This file is listed in the typings element in the package.json file.
 // To make that work, this file must be recognized as a "module".
 // For this, the file must have an export statement.
-export default JL;
+export { JL };
